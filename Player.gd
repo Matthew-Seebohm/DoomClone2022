ece800c7fb0d1c5fe5cd8866a80f901b099999eb
Matthew-Seebohm <7064470@schoolsnet.act.edu.au>
2022-08-22 14:15:20 -0700

    Fixes and Environment
    fixed the camera-to-mouse issue, turns out I forgot to include the line that stops the camera moving when the mouse stops moving. the first room for the environment was mostly finished. also finished the controls for the player and mixed around with the speed and jump strength. still have to figure out how to make the arrow disappear when in game so the arrow doesn't go off screen.

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody
 
 #Physics
-var movementSpeed = 1.0
-var jumpStrength = 1.0
+var movementSpeed = 6.0
+var jumpStrength = 3.5
 var gravity = 10.0
 
 #Cam look
@@ -27,4 +27,26 @@ func _process (delta):
 	camera.rotation_degrees -= Vector3(rad2deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
 	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minCamVerticalAngle, maxCamVerticalAngle)
 	rotation_degrees -= Vector3(0, rad2deg(mouseDelta.x), 0) * lookSensitivity * delta
- 
+	mouseDelta = Vector2()
+
+func _physics_process (delta):
+	playerVelocity.x = 0
+	playerVelocity.z = 0
+	var input = Vector2()
+	if Input.is_action_pressed("player_forward"):
+		input.y -= 1
+	if Input.is_action_pressed("player_backward"):
+		input.y += 1
+	if Input.is_action_pressed("player_left"):
+		input.x -= 1
+	if Input.is_action_pressed("player_right"):
+		input.x += 1
+	input = input.normalized()
+	var forward = global_transform.basis.z
+	var right = global_transform.basis.x
+	playerVelocity.z = (forward * input.y + right * input.x).z * movementSpeed
+	playerVelocity.x = (forward * input.y + right * input.x).x * movementSpeed
+	playerVelocity.y -= gravity * delta
+	playerVelocity = move_and_slide(playerVelocity, Vector3.UP)
+	if Input.is_action_pressed("jump") and is_on_floor():
+		playerVelocity.y = jumpStrength
